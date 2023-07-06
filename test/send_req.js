ed23521a38bab9d56daf06be1e858bf4cf8596f1
Amritanshu <amritanshusingh3@gmail.com>
2023-07-07 01:12:25 +0530

    added validator to send_req module

--- test/send_req.js
+++ test/send_req.js
@@ -4,13 +4,17 @@ const api_request = async () => {
   const url = "http://localhost:3000/identify"; // Replace with the appropriate URL
 
   const requestBody = {
-    email: "example@example.com", // Replace with the email or phoneNumber you want to identify
-    phoneNumber: 9947583299,
+    email: "cat002@example.com", // Replace with the email or phoneNumber you want to identify
+    phoneNumber: 3777771235,
   };
 
   try {
     const response = await axios.post(url, requestBody);
-    console.log("Response:", response.data);
+    if (response.data && typeof response.data === "object") {
+      console.log("Response:", response.data);
+    } else {
+      console.error("Error: Invalid response format");
+    }
   } catch (error) {
     console.error("Error:", error.message);
   }
